e20c7fc902509bfffebe59b74ecccb850902b67c
Alexander Kiel <alexanderkiel@gmx.net>
2011-06-17 15:49:28 +0200

    Can index very big access logs (1 GB). Have already indexed more can 10 million log entries.

--- src/glassfish_elasticsearch/access-log.clj
+++ src/glassfish_elasticsearch/access-log.clj
@@ -39,7 +39,7 @@
   (complex [_ open-bracket-lit
             date-time (rep* (except anything close-bracket-lit))
             _ close-bracket-lit]
-    (str (parse-date-time (apply-str date-time)))))
+    (parse-date-time (apply-str date-time))))
 
 (def request-line-parser
   (complex [_ quote-lit
@@ -71,23 +71,59 @@
             _ (rep* anything)]
     (make-clf-entry host ident user date-time request-line response-code response-size)))
 
-(defn- index-line [line]
-  (first (clf-parser {:remainder line})))
+(def time-id-formatter (formatter "yyyy-MM-dd'T'HH-mm-ss"))
 
-(defn index-log-entry [log-entry]
-  (let [base-uri (first *command-line-args*)
-        body (json-str log-entry)
-        id (md5-sum body)]
-    (if base-uri
-      (try
-        (client/put (str base-uri "/glassfish-access-log-prod/test/" id) {:body body})
-        (catch java.lang.Exception e (println "Exception: " (.getMessage e)))))))
+; The id-atom holds the current date time and count value for building unique
+; ids which start with the date time followed by a counter value.
+(def id-atom (atom {:date-time nil :count nil}))
 
-(defn- index-log-file [reader]
-  "Indexes the contents of a log file. This function requires an open reader of the log file."
+(defn- inc-id [current-id date-time]
+  "Increments the value of the id-atom.
+
+   Takes the current value of the atom (current-id) and the new date time value
+   (date-time).
+
+   Increments the counter value if the current date-time is equal to the new
+   date-time. Otherwise sets the new date time and resets the counter value to
+   zero."
+  (let [current-date-time (:date-time current-id)
+        current-count (:count current-id)]
+    (if (= current-date-time date-time)
+      {:date-time current-date-time :count (inc current-count)}
+      {:date-time date-time :count 0})))
+
+(defn- build-id [date-time]
+  "Build the id of the log-entry using the supplyed date time value.
+
+   Ids consits of the date time value formatted using the time-id-formatter and
+   and additional counter value to make the ids unique. This function uses the
+   id-atom to increment the counter value."
+  (let [id (swap! id-atom inc-id date-time)]
+    (str (unparse time-id-formatter (:date-time id)) "_" (:count id))))
+
+(defn- index-log-entry [log-entry base-uri type]
+  (let [id (build-id (:date-time log-entry))
+        uri (str base-uri "/glassfish-access-log/" type "/" id)
+        body (json-str (merge log-entry {:date-time (str (:date-time log-entry))}))]
+    (try
+      (client/put uri {:body body})
+      (catch java.lang.Exception e (println "Exception while sending the JSON to elasticsearch [" uri "]:" (.getMessage e) "body:" body)))))
+
+(defn- index-line [base-uri type line]
+  (index-log-entry (rule-match clf-parser prn prn {:remainder line}) base-uri type))
+
+(defn- index-log-file [reader base-uri type]
+  "Indexes the contents of a log file. This function requires an open reader of
+   the log file and the base URI of a elasticsearch node."
   (let [lines (line-seq reader)]
-    (doall (map println (map index-log-entry (map index-line lines))))))
+    (reduce = (map (partial index-line base-uri type) lines))))
 
-(when *command-line-args*
-  (with-open [reader (reader (second *command-line-args*))]
-    (index-log-file reader)))
+(let [base-uri (nth *command-line-args* 0)
+      type (nth *command-line-args* 1)
+      filename (nth *command-line-args* 2)]
+  (do
+    (println "Index contents of:" filename)
+    (println "as type:" type)
+    (println "to:" base-uri)
+    (with-open [reader (reader filename)]
+      (index-log-file reader base-uri type))))
